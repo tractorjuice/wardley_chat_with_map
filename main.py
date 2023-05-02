7aa093797da55cdda5f8815ffbd4bd877266bba0
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-05-02 10:12:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import streamlit as st
 import requests
 from streamlit_chat import message
 import openai
-import json
-import re
 import requests
 
 API_ENDPOINT = "https://api.onlinewardleymaps.com/v1/maps/fetch?id="
@@ -16,160 +14,6 @@ html_temp = """
                 
                 </div>
                 """
-
-def swap_xy(xy):
-  new_xy = re.findall("\[(.*?)\]", xy)
-  if new_xy:
-    match = new_xy[0]
-    match = match.split(sep = ",")
-    match = match[::-1]
-    new_xy = ('[' + match[0].strip() + ',' + match[1] + ']')
-    return (new_xy)
-  else:
-    new_xy=""
-    return (new_xy)
-
-def parse_wardley_map(map_text):
-    lines = map_text.strip().split("\n")
-    title, evolution, anchors, components, nodes, links, evolve, pipelines, pioneers, market, blueline, notes, annotations, comments, style = [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
-    current_section = None
-
-    for line in lines:
-        if line.startswith("//"):
-            comments.append(line)
-
-        elif line.startswith("evolution"):
-            evolution.append(line)
-
-        elif "+<>" in line:
-            blueline.append(line)
-
-        elif line.startswith("title"):
-            name = ' '.join(line.split()[1:])
-            title.append(name)
-
-        elif line.startswith("anchor"):
-            name = line[line.find(' ') + 1:line.find('[')].strip()
-            anchors.append(name)
-
-        elif line.startswith("component"):
-            stage = ""
-            pos_index = line.find("[")
-            if pos_index != -1:
-                new_c_xy = swap_xy(line)
-                number = json.loads(new_c_xy)
-                if 0 <= number[0] <= 0.17:
-                    stage = "genesis"
-                elif 0.18 <= number[0] <= 0.39:
-                    stage = "custom"
-                elif 0.31 <= number[0] <= 0.69:
-                    stage = "product"
-                elif 0.70 <= number[0] <= 1.0:
-                    stage = "commodity"
-                else:
-                    visibility = ""
-                if 0 <= number[1] <= 0.20:
-                    visibility = "low"
-                elif 0.21 <= number[1] <= 0.70:
-                    visibility = "medium"
-                elif 0.71 <= number[1] <= 1.0:
-                    visibility = "high"
-                else:
-                    visibility = ""               
-            else:
-                new_c_xy = ""
-
-            name = line[line.find(' ') + 1:line.find('[')].strip()
-
-            label_index = line.find("label")
-            if label_index != -1:
-                label = line[label_index+len("label")+1:]
-                label = swap_xy(label)
-            else:
-                label = ""
-
-            components.append({"name": name, "description": "", "evolution": stage, "visibility": visibility, "positionxy": new_c_xy, "labelxy": label})
-
-        elif line.startswith("pipeline"):
-            new_c_xy = swap_xy(line)
-            name = line[line.find(' ') + 1:line.find('[')].strip()
-            pipelines.append({"name": name, "description": "", "positionxy": new_c_xy, "labelxy": ""})
-
-        elif line.startswith("links"):
-            links.append(line)
-
-        elif line.startswith("evolve"):
-            new_c_xy = swap_xy(line)
-            name = re.findall(r'\b\w+\b\s(.+?)\s\d', line)[0]
-            label_index = line.find("label")
-            if label_index != -1:
-                label = line[label_index+len("label")+1:]
-            else:
-                label = ""
-            label = swap_xy(label)
-            evolve.append({"name": name, "description": "", "positionxy": new_c_xy, "labelxy": label})
-
-        elif line.startswith("pioneer"):          
-            pioneers.append(line)
-
-        elif line.startswith("note"):
-            name = line[line.find(' ') + 1:line.find('[')].strip()
-            pos_index = line.find("[")
-            if pos_index != -1:
-                new_c_xy = swap_xy(line)
-            else:
-                new_c_xy = ""
-            notes.append({"name": name, "description": "", "positionxy": new_c_xy, "labelxy": ""})   
-                  
-        elif line.startswith("annotations"):
-            new_c_xy = swap_xy(line)
-            annotations.append({"name": line, "description": "", "positionxy": new_c_xy})
-
-        elif line.startswith("annotation"):
-            new_c_xy = swap_xy(line)
-            number = re.findall(r'\d+', line)
-            name = line[line.index(']')+1:].lstrip()
-            annotations.append({"number": number[0], "name": name, "description": "", "positionxy": new_c_xy})
-
-        elif line.startswith("market"):
-            name = line[line.find(' ') + 1:line.find('[')].strip()
-            new_c_xy = swap_xy(line)
-            label_index = line.find("label")
-            if label_index != -1:
-                label = line[label_index+len("label")+1:]
-            else:
-                label = ""
-            label = swap_xy(label)
-            market.append({"name": name, "description": "", "positionxy": new_c_xy, "labelxy": label})
-
-        elif line.startswith("style"):
-            style.append(line)
-
-        elif "->" in line:
-            source, target = line.strip().split("->")
-            source = source.strip()
-            target = target.strip()
-            links.append({"source": source, "target": target})
-        else:
-            continue
-
-    return {
-        "title" : title,
-        "anchors" : anchors,
-        "evolution" : evolution,
-        "components": components,
-        "links": links,
-        "evolve": evolve,
-        "markets": market,
-        "pipelines": pipelines,
-        "pioneers": pioneers,
-        "notes": notes,
-        "blueline": blueline,
-        "style": style,
-        "annotations": annotations,
-        "comments": comments,
-    }
-
 
 st.set_page_config(page_title="Chat with your Wardley Map")
 st.sidebar.title("Chat with Map")
@@ -186,13 +30,7 @@ def get_initial_message():
     response = requests.get(url)
     map_data = response.json()
     map_text = map_data["text"]
-    
-    # Parse the Wardley map text
-    parsed_map = parse_wardley_map(map_text)
-    parsed_map_str = json.dumps(parsed_map, indent=2)
-
-    #st.session_state['map_text'] = map_text
-    st.session_state['map_text'] = parsed_map_str
+    st.session_state['map_text'] = map_text
     
     messages = [
         {
